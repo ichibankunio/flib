f33edee2ba17a189d3a4626d9b713e84774b3d17
ichibankunio <kunio.ichiban@gmail.com>
2023-03-15 15:39:19 +0900

    added

--- touch.go
+++ touch.go
@@ -1,6 +1,9 @@
 package flib
 
-import "github.com/hajimehoshi/ebiten/v2"
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+)
 
 var justPressedTouchIDs []ebiten.TouchID
 var touchIDs []ebiten.TouchID
@@ -16,4 +19,15 @@ func GetTouchIDs() []ebiten.TouchID {
 
 func GetJustReleasedTouchIDs() []ebiten.TouchID {
 	return justReleasedTouchIDs
+}
+
+func IsThereJustReleasedTouch(x0, y0, x1, y1 int) bool {
+	for i := 0; i < len(justReleasedTouchIDs); i++ {
+		x, y := inpututil.TouchPositionInPreviousTick(justReleasedTouchIDs[i])
+		if x > x0 && x < x1 && y > y0 && y < y1 {
+			return true
+		}
+	}
+	
+	return false
 }
